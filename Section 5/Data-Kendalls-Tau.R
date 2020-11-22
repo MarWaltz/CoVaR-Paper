22d7194e33ee359e9daca53dd65b0c4515ae5e71
MarWaltz <67819196+MarWaltz@users.noreply.github.com>
2020-11-22 13:25:47 +0100

    Update Data-Kendalls-Tau.R

--- Section 5/Data-Kendalls-Tau.R
+++ Section 5/Data-Kendalls-Tau.R
@@ -1,7 +1,7 @@
 #Note: The calculations in this script take only a couple of seconds on an Intel(R) Xeon(R)
 #      Gold 6136 CPU with 3.00 GHz on a 64-Bit Windows Server 2016 with 24 processors.
 
-#load BTC, LTC, XMR, XRP from 'https://github.com/MarWaltz/Thesis/tree/master/Section 5/Data'
+#load BTC, LTC, XMR, XRP from 'https://github.com/MarWaltz/CoVaR-Paper/tree/master/Section 5/Data'
 rBTC = diff(log(BTC))
 rLTC = diff(log(LTC))
 rXMR = diff(log(XMR))
