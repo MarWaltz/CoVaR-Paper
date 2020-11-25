5e3670ad73234037b5233f659cd4dd9640632053
MarWaltz <67819196+MarWaltz@users.noreply.github.com>
2020-11-25 07:45:08 +0100

    Update SimStudy.R

--- Section 4/SimStudy.R
+++ Section 4/SimStudy.R
@@ -1,4 +1,4 @@
-#Note: The calculations in this file take approximately 1h on an Intel(R) Xeon(R) 
+#Note: The calculations in this file take approximately 1.5h on an Intel(R) Xeon(R) 
 #      Gold 6136 CPU with 3.00 GHz on a 64-Bit Windows Server 2016 with 24 processors.
 
 library("copula")
